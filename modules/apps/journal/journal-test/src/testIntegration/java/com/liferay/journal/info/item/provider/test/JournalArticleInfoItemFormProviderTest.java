39505a5a43c30851d9a9e4fbd440ef3f21a796f8
balazssk <balazs.safrany-kovalik@liferay.com>
2022-01-10 14:23:41 +0100

    LPS-144732 Test - author and lastEditor ProfileImages are now always added

--- modules/apps/journal/journal-test/src/testIntegration/java/com/liferay/journal/info/item/provider/test/JournalArticleInfoItemFormProviderTest.java
+++ modules/apps/journal/journal-test/src/testIntegration/java/com/liferay/journal/info/item/provider/test/JournalArticleInfoItemFormProviderTest.java
@@ -306,7 +306,7 @@ public class JournalArticleInfoItemFormProviderTest {
 			infoItemFieldValues.getInfoFieldValues();
 
 		Assert.assertEquals(
-			infoFieldValues.toString(), 19, infoFieldValues.size());
+			infoFieldValues.toString(), 21, infoFieldValues.size());
 
 		InfoFieldValue<Object> descriptionInfoFieldValue =
 			infoItemFieldValues.getInfoFieldValue("description");
