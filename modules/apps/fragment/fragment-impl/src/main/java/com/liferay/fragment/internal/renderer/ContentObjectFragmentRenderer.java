bd82f78be6c9ccdf0426e22a75a6c76056d1ccf8
balazssk <balazs.safrany-kovalik@liferay.com>
2023-11-09 11:58:04 +0100

    LPS-201295 Both warnings must be only displayed in Edit mode

--- modules/apps/fragment/fragment-impl/src/main/java/com/liferay/fragment/internal/renderer/ContentObjectFragmentRenderer.java
+++ modules/apps/fragment/fragment-impl/src/main/java/com/liferay/fragment/internal/renderer/ContentObjectFragmentRenderer.java
@@ -222,18 +222,17 @@ public class ContentObjectFragmentRenderer implements FragmentRenderer {
 		}
 
 		if (!_hasPermission(httpServletRequest, className, displayObject)) {
-			if (!FeatureFlagManagerUtil.isEnabled("LPS-169923")) {
-				FragmentRendererUtil.printPortletMessageInfo(
-					httpServletRequest, httpServletResponse,
-					"you-do-not-have-permission-to-access-the-requested-" +
-						"resource");
-
-				return;
-			}
-
 			if (FragmentRendererUtil.isEditMode(httpServletRequest)) {
-				FragmentRendererUtil.printRestrictedContentMessage(
-					httpServletRequest, httpServletResponse);
+				if (!FeatureFlagManagerUtil.isEnabled("LPS-169923")) {
+					FragmentRendererUtil.printPortletMessageInfo(
+						httpServletRequest, httpServletResponse,
+						"you-do-not-have-permission-to-access-the-requested-" +
+							"resource");
+				}
+				else {
+					FragmentRendererUtil.printRestrictedContentMessage(
+						httpServletRequest, httpServletResponse);
+				}
 			}
 
 			return;
