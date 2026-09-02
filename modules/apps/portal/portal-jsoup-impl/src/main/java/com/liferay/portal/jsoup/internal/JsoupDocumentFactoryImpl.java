33f8c41d9442fb73c427742f9c830bf0437d0375
balazssk <balazs.safrany-kovalik@liferay.com>
2026-09-02 13:10:42 +0200

    LPD-103665 Add the legacy self-closing tag parsing option

--- modules/apps/portal/portal-jsoup-impl/src/main/java/com/liferay/portal/jsoup/internal/JsoupDocumentFactoryImpl.java
+++ modules/apps/portal/portal-jsoup-impl/src/main/java/com/liferay/portal/jsoup/internal/JsoupDocumentFactoryImpl.java
@@ -5,27 +5,90 @@
 
 package com.liferay.portal.jsoup.internal;
 
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.jsoup.JsoupDocumentFactory;
+import com.liferay.portal.jsoup.configuration.JsoupConfiguration;
+
+import java.util.Map;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.parser.Parser;
+import org.jsoup.parser.Tag;
+import org.jsoup.parser.TagSet;
 
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Modified;
 
 /**
  * @author Balázs Sáfrány-Kovalik
  */
-@Component(service = JsoupDocumentFactory.class)
+@Component(
+	configurationPid = "com.liferay.portal.jsoup.configuration.JsoupConfiguration",
+	service = JsoupDocumentFactory.class
+)
 public class JsoupDocumentFactoryImpl implements JsoupDocumentFactory {
 
 	@Override
 	public Document parse(String html) {
-		return _disablePrettyPrint(Jsoup.parse(html));
+		Parser parser = _getLegacySelfClosingTagParserInstance();
+
+		if (parser == null) {
+			return _disablePrettyPrint(Jsoup.parse(html));
+		}
+
+		return _disablePrettyPrint(
+			_clearSelfCloseFlags(Jsoup.parse(html, StringPool.BLANK, parser)));
 	}
 
 	@Override
 	public Document parseBodyFragment(String html) {
-		return _disablePrettyPrint(Jsoup.parseBodyFragment(html));
+		Parser parser = _getLegacySelfClosingTagParserInstance();
+
+		if (parser == null) {
+			return _disablePrettyPrint(Jsoup.parseBodyFragment(html));
+		}
+
+		return _disablePrettyPrint(
+			_clearSelfCloseFlags(_parseBodyFragmentWithParser(html, parser)));
+	}
+
+	@Activate
+	@Modified
+	protected void activate(Map<String, Object> properties) {
+		JsoupConfiguration jsoupConfiguration =
+			ConfigurableUtil.createConfigurable(
+				JsoupConfiguration.class, properties);
+
+		if (jsoupConfiguration.legacySelfClosingTagParsing()) {
+			_legacySelfClosingTagParser = _createSelfClosingTagParser();
+		}
+		else {
+			_legacySelfClosingTagParser = null;
+		}
+	}
+
+	private Document _clearSelfCloseFlags(Document document) {
+		for (Element element : document.getAllElements()) {
+			Tag tag = element.tag();
+
+			tag.clear(Tag.SelfClose);
+		}
+
+		return document;
+	}
+
+	private Parser _createSelfClosingTagParser() {
+		Parser parser = Parser.htmlParser();
+
+		TagSet tagSet = parser.tagSet();
+
+		tagSet.onNewTag(tag -> tag.set(Tag.SelfClose));
+
+		return parser;
 	}
 
 	private Document _disablePrettyPrint(Document document) {
@@ -35,5 +98,30 @@ public class JsoupDocumentFactoryImpl implements JsoupDocumentFactory {
 
 		return document;
 	}
+
+	private Parser _getLegacySelfClosingTagParserInstance() {
+		Parser parser = _legacySelfClosingTagParser;
+
+		if (parser == null) {
+			return null;
+		}
+
+		return parser.newInstance();
+	}
+
+	private Document _parseBodyFragmentWithParser(String html, Parser parser) {
+		Document document = Document.createShell(StringPool.BLANK);
+
+		document.parser(parser);
+
+		Element bodyElement = document.body();
+
+		bodyElement.appendChildren(
+			parser.parseFragmentInput(html, bodyElement, StringPool.BLANK));
+
+		return document;
+	}
+
+	private volatile Parser _legacySelfClosingTagParser;
 
 }
